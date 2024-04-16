80937f1a0990cc9cf2b63a568069d38a2c30a3b2
mangeshchavan <mangesh.chavan@tejgyan.org>
2024-04-16 20:56:39 +0530

    next auth url added

--- next.config.mjs
+++ next.config.mjs
@@ -11,6 +11,7 @@ const nextConfig = {
     KEYCLOAK_CLIENT_SECRET: process.env.KEYCLOAK_CLIENT_SECRET,
     KEYCLOAK_ISSUER: process.env.KEYCLOAK_ISSUER,
     NEXTAUTH_SECRET:process.env.NEXTAUTH_SECRET,
+    NEXTAUTH_URL:process.env.NEXTAUTH_URL,
   },
   transpilePackages: [
     "@ant-design",
