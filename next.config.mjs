2afa8102016c400aea3c82251fd282b75b7dda05
sayakka123 <sayakkaplambe@gmail.com>
2024-04-29 11:05:07 +0530

    add notification functionality

--- next.config.mjs
+++ next.config.mjs
@@ -11,8 +11,8 @@ const nextConfig = {
     KEYCLOAK_CLIENT_SECRET: process.env.KEYCLOAK_CLIENT_SECRET,
     KEYCLOAK_ISSUER: process.env.KEYCLOAK_ISSUER,
     NEXTAUTH_SECRET:process.env.NEXTAUTH_SECRET,
-    // NEXTAUTH_URL:process.env.NEXTAUTH_URL,
-    NEXTAUTH_URL: process.env.NODE_ENV === 'production' ? process.env.NEXTAUTH_URL_PROD : process.env.NEXTAUTH_URL_DEV,
+    NEXTAUTH_URL:process.env.NEXTAUTH_URL,
+    // NEXTAUTH_URL: process.env.NODE_ENV === 'production' ? process.env.NEXTAUTH_URL_PROD : process.env.NEXTAUTH_URL_DEV,
 
   },
   transpilePackages: [
